b50628b81c2be01be5bc9927b4cae08b344ff680
Mk1 <clear.mt.everest@gmail.com>
2022-06-14 04:48:33 +0900

    refs #1 add SimpleParser

--- app/src/test/java/BlueChartSolver/app/SimpleParserTest.java
+++ app/src/test/java/BlueChartSolver/app/SimpleParserTest.java
@@ -3,7 +3,6 @@ package BlueChartSolver.app;
 import BlueChartSolver.app.Parser.SimpleParser;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -12,18 +11,23 @@ public class SimpleParserTest {
 
     @Test
     public void testAssertion() {
-        assertThrows(AssertionError.class, () -> parser.parse(""));
-        assertThrows(AssertionError.class, () -> parser.parse("+"));
-        assertThrows(AssertionError.class, () -> parser.parse("+ 100"));
-        assertDoesNotThrow(() -> parser.parse("5"));
-        assertDoesNotThrow(() -> parser.parse("-5"));
-        assertDoesNotThrow(() -> parser.parse("-5abc"));
-        assertDoesNotThrow(() -> parser.parse("-5abc^2"));
-        assertDoesNotThrow(() -> parser.parse("-5abc^2 + 100"));
+        assertThrows(IllegalArgumentException.class, () -> parser.parse(""));
+        assertThrows(IllegalArgumentException.class, () -> parser.parse("+"));
+        assertThrows(IllegalArgumentException.class, () -> parser.parse("+ 100"));
     }
 
     @Test
-    public void test() {
-        assertEquals("2x", parser.parse("x + x").toString());
+    public void parseSpaceSeparatedText() {
+        var x = Variable.named('x');
+        var f1 = x.powerOf(2).times(-1)
+                .plus(x.times(5))
+                .minus(4);
+        assertEquals(f1, parser.parse("-x^2 + 5x - 4"));
+
+        var y = Variable.named('y');
+        var f2 = x.powerOf(2).times(-5)
+                .plus(x.times(4).times(y))
+                .minus(y.powerOf(2).times(4));
+        assertEquals(f2, parser.parse("-5x^2 + 4xy - 4y^2"));
     }
 }
