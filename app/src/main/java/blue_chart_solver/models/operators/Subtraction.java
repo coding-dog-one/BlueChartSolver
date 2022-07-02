c70a6ea15de236e1ddf7d592501ef8398ff9b693
Mk1 <clear.mt.everest@gmail.com>
2022-07-03 04:15:53 +0900

    refs #15 Add logger to operators.

--- app/src/main/java/blue_chart_solver/models/operators/Subtraction.java
+++ app/src/main/java/blue_chart_solver/models/operators/Subtraction.java
@@ -1,10 +1,14 @@
 package blue_chart_solver.models.operators;
 
 import blue_chart_solver.models.Polynomial;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Subtraction implements Operator {
+    private static final Logger logger = LoggerFactory.getLogger(Subtraction.class);
     @Override
     public Polynomial operate(Polynomial f1, Polynomial f2) {
+        logger.debug("operate: ({}) - ({})", f1, f2);
         return f1.minus(f2);
     }
 }
