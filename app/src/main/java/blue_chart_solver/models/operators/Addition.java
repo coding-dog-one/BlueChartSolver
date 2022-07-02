09118852283ef750d11c08c2270569b1bdb4e738
Mk1 <clear.mt.everest@gmail.com>
2022-07-03 04:15:53 +0900

    refs #15 Add logger to operators.

--- app/src/main/java/blue_chart_solver/models/operators/Addition.java
+++ app/src/main/java/blue_chart_solver/models/operators/Addition.java
@@ -1,11 +1,15 @@
 package blue_chart_solver.models.operators;
 
 import blue_chart_solver.models.Polynomial;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Addition implements Operator {
+    private static final Logger logger = LoggerFactory.getLogger(Addition.class);
 
     @Override
     public Polynomial operate(Polynomial f1, Polynomial f2) {
+        logger.debug("Operate: ({}) + ({})", f1, f2);
         return f1.plus(f2);
     }
 }
--- app/src/main/java/blue_chart_solver/models/operators/Multiply.java
+++ app/src/main/java/blue_chart_solver/models/operators/Multiply.java
@@ -1,10 +1,14 @@
 package blue_chart_solver.models.operators;
 
 import blue_chart_solver.models.Polynomial;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Multiply implements Operator {
+    private static final Logger logger = LoggerFactory.getLogger(Multiply.class);
     @Override
     public Polynomial operate(Polynomial f1, Polynomial f2) {
+        logger.debug("Operate: ({}) * ({})", f1, f2);
         return f1.times(f2);
     }
 }
